916fc9a32e46804d57125d1c4905ce1b44f0606a
Shaddam Amru Hasibuan <shaddamamru@airmasgroup.co.id>
2026-06-02 16:47:15 +0700

    feat: add post release scan action, fix release workflow, remove unused package

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  api.cache(true);
+  const reanimatedPlugin =
+    process.env.NODE_ENV === "production"
+      ? ["react-native-reanimated/plugin", { loose: true }]
+      : "react-native-reanimated/plugin";
+
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      reanimatedPlugin,
+    ],
+  };
+};
